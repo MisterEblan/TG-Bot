403e458f6dcfc756da610540bfa5554343c7a2bd
Danil <boy4rov.da@gmail.com>
2024-08-20 14:47:36 +0300

    Made a reversed echo bot with logging

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 
 
-BOT_TOKEN = '7501521604:AAEsNguT8xFRr_gkdKM9OKJQMcT3hoggE9A'
+from config import BOT_TOKEN
+
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
@@ -13,14 +14,17 @@ bot = Bot(token=BOT_TOKEN)
 # Диспетчер
 dp = Dispatcher()
 
+
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer("Hello!")
     
 @dp.message()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+async def reversedEcho(message: types.Message):
+    print(message.text)
+    reply = message.text [::-1]
+    await message.answer(reply)
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
